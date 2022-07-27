b81d2d7bf6f55151c03f3b6b95ee37e81aae0942
enod <win_1118@hotmail.com>
2022-07-27 12:51:28 -0500

    Added comments, cleaned all the console logs

--- js/app.js
+++ js/app.js
@@ -10,29 +10,29 @@ let rainbow = false;
 
 appendDivs=(element=4)=>{                                       //Function that appends as much divs as desired, initially 16 if not element passed
 
-    divsNumber= element * element;
-    
-    for (i = 0; i < divsNumber ; i++){
+    divsNumber= element * element;                              //Takes the selected number(default 4) and multiples it by itself
+                                                                //To get the total divs to make
+    for (i = 0; i < divsNumber ; i++){                          //Make the divs and append each one with the class 'pixel'
         const div = document.createElement('div');
         div.classList.add('pixel');
         container.appendChild(div);
     }
 
-    let frNumb = ''
-    for(i=0; i<element; i++){
+    let frNumb = ''                                             //Auxiliar for the rows and columns operation
+    for(i=0; i<element; i++){                                   //For each side add 1fr, referring to the columns and rows that the new item will have
         frNumb += ' 1fr'
     }
 
-    container.style.gridTemplateColumns= frNumb;
-    container.style.gridTemplateRows=frNumb;
-    divs = document.querySelectorAll('.pixel');
+    container.style.gridTemplateColumns= frNumb;                //Add number of columns
+    container.style.gridTemplateRows=frNumb;                    //Add number of rows
+    divs = document.querySelectorAll('.pixel');                 //Select all the made divs
     
-    divs.forEach(item=>{
+    divs.forEach(item=>{                                        //Add a function to each one to detect the mouse hovering on them
         item.addEventListener('mouseover',e=>{
             e.stopPropagation();
-            if(rainbow==false){
-            e.target.style.backgroundColor= selectedColor;
-            } else if(rainbow ==true){
+            if(rainbow==false){                                 //When the mouse hover over change it color depending on two parameters
+            e.target.style.backgroundColor= selectedColor;      //If the rainbow button its disabled change the pixel color to the selected color via the input color
+            } else if(rainbow ==true){                          //If the rainbow button its enabled change the color to a random color
             e.target.style.backgroundColor= '#' + Math.floor(Math.random()*16777215).toString(16);              
             }
         })
@@ -41,31 +41,28 @@ appendDivs=(element=4)=>{                                       //Function that 
 
 appendDivs()
 
-btnNewGrid.addEventListener('click',(e)=>{
-    let grid = prompt('Ingresa el numero de cuadros por lado que quieres en tu nuevo lienzo (Menor a 100)');
-    grid =parseInt(grid);
-    console.log(typeof grid)
-    console.log(grid)
-    if (grid > 100 || Number.isNaN(grid)){
-        alert("Por favor introduzca un parametro válido (Un número positivo menor a 100)")
-    }else{
+btnNewGrid.addEventListener('click',(e)=>{                                      //Function that checks if the button for a new grid has ben pressed
+    let grid = prompt('Ingresa el numero de cuadros por lado que quieres en tu nuevo lienzo (Menor a 100)');    //Prompt for data of a new grid
+    grid =parseInt(grid);                                                       //Transform that data into a number
+    if (grid > 100 || Number.isNaN(grid)){                                      //Check if the number is larger than 100 or is not a number
+        alert("Por favor introduzca un parametro válido (Un número positivo menor a 100)") //If incorrect data has been inputed send an alert
+    }else{                                                                      //If the number computes proced with the grid creation
         container.innerHTML = '';
         appendDivs(grid);
     }
     
 })
 
-btnReset.addEventListener('click',e=>{
+btnReset.addEventListener('click',e=>{                  //Reset the grid
     container.innerHTML = '';
     appendDivs();
 })
 
-color.addEventListener('change',e=>{
+color.addEventListener('change',e=>{                    //Change the color for the normal brush
     selectedColor = color.value;
-    console.log(selectedColor);
 })
 
-rainbowButton.addEventListener('click',e=>{
+rainbowButton.addEventListener('click',e=>{             //Enables or disables the rainbow mod
     if(rainbow == true){
         rainbow = false;
         rainbowButton.innerText = "Rainbow Off"
