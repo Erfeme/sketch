e8b4d5ef39e4c6c0756d44ce045c9dabfa1d3c50
Enod <win_1118@hotmail.com>
2022-07-26 22:16:22 -0500

    Added a function that appends as much divs as a passed value

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
-body = document.querySelector('body');
 const container = document.querySelector('.gridContainer');
+let divs = ''
 
-appendDivs=div=>{
+appendDivs=(element=16)=>{                                       //Function that appends as much divs as desired, initially 16 if not element passed
 
     for (i = 0; i < 16 ; i++){
         const div = document.createElement('div');
         div.classList.add('pixel');
         container.appendChild(div);
     }
-
+    divs = document.querySelectorAll('.pixel');
 }
 
 appendDivs()
+
