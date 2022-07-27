2eeb6a9987e2bbc76f2957c7fd635b67d73c6308
Enod <win_1118@hotmail.com>
2022-07-26 22:49:00 -0500

    Added a Button,it's class is newGrid when presed it prompts a window asking for the new size of the square, it triggers the appendDivs function, that also now modifies the row and columns to match the new size without losing proportion of the square

--- js/app.js
+++ js/app.js
@@ -1,15 +1,37 @@
 const container = document.querySelector('.gridContainer');
-let divs = ''
+const btnNewGrid = document.querySelector('.newGrid');
+let divs = '';
 
-appendDivs=(element=16)=>{                                       //Function that appends as much divs as desired, initially 16 if not element passed
+appendDivs=(element=4)=>{                                       //Function that appends as much divs as desired, initially 16 if not element passed
 
-    for (i = 0; i < 16 ; i++){
+
+    divsNumber= element * element;
+    
+    for (i = 0; i < divsNumber ; i++){
         const div = document.createElement('div');
         div.classList.add('pixel');
         container.appendChild(div);
     }
+
+    let frNumb = ''
+    for(i=0; i<element; i++){
+        frNumb += ' 1fr'
+    }
+    console.log(frNumb);
+    
+    container.style.gridTemplateColumns= frNumb;
+    container.style.gridTemplateRows=frNumb;
+
+    
+
     divs = document.querySelectorAll('.pixel');
 }
 
 appendDivs()
+
+btnNewGrid.addEventListener('click',(e)=>{
+    let grid = prompt('Please enter the squares per side that you want in the new grid, a number minor than 100');
+    container.innerHTML = '';
+    appendDivs(grid);
+})
 
